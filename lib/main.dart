ffb6e5af47e4256d6bed0935fbb4b6cbc1cd9231
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-12-06 16:54:33 -0300

    Remoção do teste com inheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shop/pages/counter_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
-import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
 
 void main() {
@@ -15,20 +14,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewPage(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => CounterPage()
-        },
-        debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
+      home: ProductsOverviewPage(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage()
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
