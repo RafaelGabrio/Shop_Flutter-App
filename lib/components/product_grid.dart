92f7cfec67890594dc9567f753fc18a1fc7c6136
Rafael Gábrio <rafael.santos@itsolved.com.br>
2021-12-16 10:41:01 -0300

    refatoração do código para a criação da nova widget ProductGrid

---
+++ lib/components/product_grid.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
+
+class ProductGrid extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
