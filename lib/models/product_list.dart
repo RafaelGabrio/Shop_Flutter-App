e972788eba78ba4c619bba053121f51b4537f071
Rafael Gábrio <rafael.santos@itsolved.com.br>
2022-03-09 14:45:03 -0300

    adicionado o filtro de exibir apenas favoritos na tela inicial utilizando o provider

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -4,8 +4,24 @@ import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
+  bool _showFavoriteOnly = false;
 
-  List<Product> get items => [..._items];
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
+
+  List<Product> get items {
+    if(_showFavoriteOnly) {
+      return _items.where((prod) => prod.isFavorite).toList();
+    }
+    return [..._items];
+  }
   
   void addProduct(Product product) {
     _items.add(product);
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -5,14 +5,43 @@ import 'package:shop/components/product_grid.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/models/product_list.dart';
 
+
+enum FilterOptions {
+  Favorite,
+  All
+}
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: ProductGrid(),
     );
