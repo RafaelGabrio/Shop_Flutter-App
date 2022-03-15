0503b8bd7d26d41a2cc632e3b824743195995618
Rafael Gábrio <rafael-sims@live.com>
2022-03-15 11:19:48 -0300

    criando provider do Cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
@@ -15,8 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -25,9 +33,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         home: ProductsOverviewPage(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage()
-        },
+        routes: {AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailPage()},
         debugShowCheckedModeBanner: false,
       ),
     );
