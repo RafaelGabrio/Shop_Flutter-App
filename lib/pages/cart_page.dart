9ca821ce32440e61ff62c09519fce9abbe08c8d1
Rafael Gábrio <rafael-sims@live.com>
2022-03-17 21:08:37 -0300

    página do carrinho feita

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Carrinho'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Carrinho'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              margin: const EdgeInsets.all(25),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Chip(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      label: Text(
+                        'R\$${cart.totalAmount}',
+                        style: TextStyle(
+                          color:
+                              Theme.of(context).primaryTextTheme.headline6?.color,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('COMPRAR'),
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
